00fa1a9dca6fb79aa05ff818aa2a1d15a3c6817a
ArthurStoliarchuk <clash4clans28@gmail.com>
2024-10-20 22:53:14 +0300

    Backend for MERN-stack project - Simple Shop

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.routes.js';
+
+dotenv.config();
+const app = express();
+
+// Middleware для парсингу JSON
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
+app.listen(5000, () => {
+   connectDB();
+   console.log('Server started at http://localhost:5000');
+});
